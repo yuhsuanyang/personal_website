1f9b74bc9326c3c2da43583b5a3cf717ea33dd50
yuhsuanyang <sb0953330882@yahoo.com.tw>
2026-01-18 11:55:03 +0800

    test: add copy email address test

--- webpage/src/__tests__/Links.test.jsx
+++ webpage/src/__tests__/Links.test.jsx
@@ -8,24 +8,34 @@ describe("Links test", () => {
     render(<App />);
   });
   it("Link Linkedin page", async () => {
-    const button = screen.getByRole("link", { name: "Linkedin" });
-    expect(button).toHaveAttribute(
+    const link = screen.getByRole("link", { name: "Linkedin" });
+    expect(link).toHaveAttribute(
       "href",
       "https://www.linkedin.com/in/darcy-yu-hsuan-yang/",
     );
+    expect(link).toHaveAttribute("target", "_blank");
   });
 
   it("Link Github page", async () => {
-    const button = screen.getByRole("link", { name: "Github" });
-    expect(button).toHaveAttribute("href", "https://github.com/yuhsuanyang");
+    const link = screen.getByRole("link", { name: "Github" });
+    expect(link).toHaveAttribute("href", "https://github.com/yuhsuanyang");
+    expect(link).toHaveAttribute("target", "_blank");
   });
 
   it("Show Email Address", async () => {
-    // const copiedTest = vi.fn(); // record function call
-    // Object.assign(navigator, { clipboard: { copiedTest } });
-    const button = screen.getByRole("link", { name: "Email" });
+    const button = screen.getByRole("button", { name: "Email" });
     await userEvent.click(button);
     expect(screen.getByText("sb0953330882@gmail.com")).toBeInTheDocument();
-    // expect(copiedTest).toHaveBeenCalledWith("sb0953330882@gmail.com");
+    expect(screen.getByRole("button", { name: "Copy" })).toBeInTheDocument();
+  });
+  it("Copy Email Address", async () => {
+    const copiedTest = vi.fn(); // record function call
+    Object.assign(navigator, { clipboard: { copiedTest } });
+    const emailButton = screen.getByRole("button", { name: "Email" });
+    await userEvent.click(emailButton);
+
+    const copyButton = screen.getByRole("button", { name: "Copy" });
+    await userEvent.click(copyButton);
+    expect(copiedTest).toHaveBeenCalledWith("sb0953330882@gmail.com");
   });
 });
